56b1ae74e9e384731784eb93373ed9ad2c654a1e
Lucia Depaoli <luciadep@gmail.com>
2023-07-25 14:34:38 +0200

    commit

--- script/requirements_dll.py
+++ script/requirements_dll.py
@@ -81,26 +81,13 @@ def find_min_t_bin(T):
 
 from keras import backend as K
 def weighted_categorical_crossentropy(weights):
-    """
-    A weighted version of keras.objectives.categorical_crossentropy
-    
-    Variables:
-        weights: numpy array of shape (C,) where C is the number of classes
-    
-    Usage:
-        weights = np.array([0.5,2,10]) # Class one at 0.5, class 2 twice the normal weights, class 3 10x.
-        loss = weighted_categorical_crossentropy(weights)
-        model.compile(loss=loss,optimizer='adam')
-    """
     
     weights = K.variable(weights)
         
     def loss(y_true, y_pred):
-        # scale predictions so that the class probas of each sample sum to 1
         y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
-        # clip to prevent NaN's and Inf's
+        
         y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
-        # calc
         loss = y_true * K.log(y_pred) * weights
         loss = -K.sum(loss, -1)
         return loss
@@ -115,11 +102,9 @@ def weighted_categorical_crossentropy_comp(weights, y_true, y_pred):
     y_pred = y_pred.astype('float32')
 
     weights = K.variable(weights)
-    # scale predictions so that the class probas of each sample sum to 1
     y_pred /= K.sum(y_pred, axis=-1, keepdims=True)
-    # clip to prevent NaN's and Inf's
+    
     y_pred = K.clip(y_pred, K.epsilon(), 1 - K.epsilon())
-    # calc
     loss = y_true * K.log(y_pred) * weights
     loss = -K.sum(loss, -1)
     return loss
@@ -130,7 +115,6 @@ def weighted_binary_crossentropy(zero_weight, one_weight):
 
         b_ce = K.binary_crossentropy(y_true, y_pred)
 
-        # weighted calc
         weight_vector = y_true * one_weight + (1 - y_true) * zero_weight
         weighted_b_ce = weight_vector * b_ce
 
